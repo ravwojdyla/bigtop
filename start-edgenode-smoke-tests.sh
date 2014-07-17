792992854dbc58554f0715e37a45e4b680430cd0
Rafal Wojdyla <rav@spotify.com>
2014-07-17 13:28:15 +0200

    Handle crunch tests

--- start-edgenode-smoke-tests.sh
+++ start-edgenode-smoke-tests.sh
@@ -14,6 +14,28 @@ export MYSQL_HOST=none
 export DB_USER=hive
 export DB_PASSWORD=hive
 
+CRUNCH_EXAMPLES_JAR="/tmp/apache-crunch-0.8.3-hadoop2-bin/crunch-examples-0.8.3-hadoop2-job.jar"
+
+function rc_check {
+    if [ $? -ne 0 ]; then
+        echo "Failure - exit!"
+        exit -1
+    fi
+}
+
+function prereq_for_crunch {
+    if [ -f $CRUNCH_EXAMPLES_JAR ]; then
+       return
+    fi
+    if [ ! -f /tmp/apache-crunch-0.8.3-hadoop2-bin.tar.gz ]; then
+        echo "Crunch tar in not available - downloading ..."
+        curl http://apache.mirrors.spacedump.net/crunch/crunch-0.8.3/apache-crunch-0.8.3-hadoop2-bin.tar.gz > /tmp/apache-crunch-0.8.3-hadoop2-bin.tar.gz
+        rc_check
+   fi
+   tar xvzf /tmp/apache-crunch-0.8.3-hadoop2-bin.tar.gz -C /tmp
+   rc_check
+}
+
 for PROJECT in "$@"
 do
 
@@ -53,6 +75,9 @@ do
         # mvn3 clean verify -D$LOG4J_LEVEL -D'db.tblname=TBLS' -D'db.dbname=meta_db' -D'db.host=hive.hdfsha.cloud.spotify.net' -D'org.apache.maven-failsafe-plugin.testInclude=**/TestSqoopImportPostgres*' -f $EXECUTION_POM_FILE
     # snakebite, luigi, httpfs
 	echo "Sqoop smoke tests are currently disabled"
+    elif [ "$PROJECT" == "crunch" ]; then
+        prereq_for_crunch
+        mvn3 clean verify -DargLine="-Dorg.apache.bigtop.itest.crunch.smoke.crunch.jar=${CRUNCH_EXAMPLES_JAR}" -D$LOG4J_LEVEL -f $EXECUTION_POM_FILE
     elif [ "$PROJECT" == "snakebite" ] || [ "$PROJECT" == "luigi" ] || [ "$PROJECT" == "httpfs" ] ; then
         mvn3 clean verify -D$LOG4J_LEVEL -f $EXECUTION_POM_FILE
     fi
