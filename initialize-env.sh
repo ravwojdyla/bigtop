e1b12e782f95edfe00cad4a0744565559b4fdedb
Rafal Wojdyla <rav@spotify.com>
2014-07-23 17:59:33 +0200

    Fix initialization process

---
+++ initialize-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+HADOOP_HOME=${HADOOP_HOME:-"/usr/lib/hadoop"}
+HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-"/etc/hadoop/conf"}
--- initialize-tests.sh
+++ initialize-tests.sh
@@ -1,6 +1,13 @@
 #/bina/bash
 
+if [ -f ./initialize-env.sh ]; then
+    . ./initialize-env.sh
+fi
+
+mvn3 clean install
 mvn3 clean install -DskipTests -DskipITs -DperformRelease -f bigtop-test-framework/pom.xml
 mvn3 clean install -DskipTests -DskipITs -DperformRelease -f bigtop-tests/test-artifacts/pom.xml
-mvn3 clean install -DskipTests -DskipITs -DperformRelease -o -nsu -f ./bigtop-test-framework/pom.xml
-mvn3 clean install -DskipTests -DskipITs -DperformRelease -o -nsu -f ./bigtop-tests/test-artifacts/pom.xml
+mvn3 clean install -DskipTests -DskipITs -DperformRelease -f bigtop-tests/test-execution/pom.xml
+mvn3 clean install -DskipTests -DskipITs -DperformRelease -o -nsu -f bigtop-test-framework/pom.xml
+mvn3 clean install -DskipTests -DskipITs -DperformRelease -o -nsu -f bigtop-tests/test-artifacts/pom.xml
+mvn3 clean install -DskipTests -DskipITs -DperformRelease -o -nsu -f bigtop-tests/test-execution/pom.xml
